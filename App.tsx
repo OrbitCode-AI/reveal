f35f91b0d051b7bb43194bdc16d3f7376ddb1875
Eric Pollmann <eric@pollmann.net>
2026-01-25 23:18:29 -0800

    Apply consistent code style to App entry point
    Remove semicolons for consistent formatting with rest of codebase.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { Deck, setTheme } from './lib/reveal';
-import { Title } from './slides/Title';
-import { Vertical } from './slides/Vertical';
-import { Intro } from './slides/Intro';
-import { Code } from './slides/Code';
-import { PointOfView } from './slides/PointOfView';
-import { AutoAnimate } from './slides/AutoAnimate';
-import { Features } from './slides/Features';
-import { Fragments } from './slides/Fragments';
-import { Transitions } from './slides/Transitions';
-import { Themes } from './slides/Themes';
-import { Background } from './slides/Background';
-import { Ending } from './slides/Ending';
+import { Deck, setTheme } from './lib/reveal'
+import { Title } from './slides/Title'
+import { Vertical } from './slides/Vertical'
+import { Intro } from './slides/Intro'
+import { Code } from './slides/Code'
+import { PointOfView } from './slides/PointOfView'
+import { AutoAnimate } from './slides/AutoAnimate'
+import { Features } from './slides/Features'
+import { Fragments } from './slides/Fragments'
+import { Transitions } from './slides/Transitions'
+import { Themes } from './slides/Themes'
+import { Background } from './slides/Background'
+import { Ending } from './slides/Ending'
 
 export default function App() {
   return (
@@ -28,5 +28,5 @@ export default function App() {
       <Background />
       <Ending />
     </Deck>
-  );
+  )
 }
