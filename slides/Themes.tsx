747bc75e2e16ce727f2a1911a1850b170c672bee
Eric Pollmann <eric@pollmann.net>
2026-02-28 21:51:36 -0800

    Reformat

--- slides/Themes.tsx
+++ slides/Themes.tsx
@@ -15,10 +15,7 @@ const themeButtonStyle = {
 
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 
-function ThemeButton({
-  theme,
-  label,
-}: { theme: string; label?: string }) {
+function ThemeButton({ theme, label }: { theme: string; label?: string }) {
   return (
     <button type="button" style={themeButtonStyle} onClick={() => setTheme(theme)}>
       {label ?? capitalize(theme)}
@@ -32,18 +29,13 @@ export function Themes({ setTheme: _setTheme }: ThemesSlideProps) {
       <h2>Themes</h2>
       <p>reveal.js comes with a few themes built in:</p>
       <p>
-        <ThemeButton theme="black" label="Black (default)" /> -{' '}
-        <ThemeButton theme="white" /> -{' '}
-        <ThemeButton theme="league" /> -{' '}
-        <ThemeButton theme="sky" /> -{' '}
-        <ThemeButton theme="beige" /> -{' '}
-        <ThemeButton theme="simple" />
+        <ThemeButton theme="black" label="Black (default)" /> - <ThemeButton theme="white" /> -{' '}
+        <ThemeButton theme="league" /> - <ThemeButton theme="sky" /> - <ThemeButton theme="beige" />{' '}
+        - <ThemeButton theme="simple" />
       </p>
       <p>
-        <ThemeButton theme="serif" /> -{' '}
-        <ThemeButton theme="blood" /> -{' '}
-        <ThemeButton theme="night" /> -{' '}
-        <ThemeButton theme="moon" /> -{' '}
+        <ThemeButton theme="serif" /> - <ThemeButton theme="blood" /> -{' '}
+        <ThemeButton theme="night" /> - <ThemeButton theme="moon" /> -{' '}
         <ThemeButton theme="solarized" />
       </p>
     </section>
